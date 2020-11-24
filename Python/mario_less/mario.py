0950e33d129e5c3fc6e6978eb97f4fc235c71067
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-11-24 16:48:25 -0300

    Mario Less in python

---
+++ Python/mario_less/mario.py
@@ -0,0 +1,19 @@
+from cs50 import get_int
+
+def get_height():
+    height = 0
+    while height <= 0 or height > 8:
+        height = get_int("Height: ")
+
+    return height
+
+
+height = get_height()
+
+for i in range(0, height):
+    for l in range(1, height - i):
+        print(" ", end="")
+
+    for j in range(0, i +1):
+        print("#", end="")
+    print()
